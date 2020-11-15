c4dea1ad822169742da7e03f2ce3f7bd66d2db9f
janu <satyasweety.455@gmail.com>
2020-11-15 19:42:51 +0530

    Modified  on Utility.java

--- HelloWorld/src/com/nt/beans/Utility.java
+++ HelloWorld/src/com/nt/beans/Utility.java
@@ -2,11 +2,10 @@ package com.nt.beans;
 
 public class Utility {
 	public void m1() {
-<<<<<<< HEAD
-		System.out.println("utility m1() method :abcd");
-=======
+
 		System.out.println("utility m1() method 1234");
->>>>>>> branch 'master' of https://github.com/satyavathisiddavatam/NITCityBank.git
+		System.out.println("abcd");
+
 	}
 
 	public static void main(String[] args) {
